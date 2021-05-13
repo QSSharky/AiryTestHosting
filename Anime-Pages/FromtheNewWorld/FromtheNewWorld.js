021b9ba0d4cf92482ba47a25c20601b328240b1b
Sharky <sharky.2019@mail.ru>
2021-05-13 17:50:17 +0700

    Версия 0.1.0! Первый релиз, добавленные страницы: "Главная", "Из нового света"!

---
+++ Anime-Pages/FromtheNewWorld/FromtheNewWorld.js
@@ -0,0 +1,155 @@
+function changingColorOfButtons() {
+    //Смена серии
+    let iframe = document.querySelector('iframe')
+    let episodeName = document.getElementById('ShinsekaiYoriEpisodeName')
+    let episodeOutDate = document.getElementById('ShinsekaiYoriEpisodeOutDate')
+    //Кнопки
+    if(true) {
+    if(this.id === 'btn1') {
+        iframe.src = 'https://www.myvi.top/embed/haot3rf3kqmwjezdkzjiterq7w'
+        episodeName.textContent = 'Название серии: Season of Fallen Leaves'
+        episodeOutDate.textContent = 'Дата выхода: 29.09.2012'
+    }
+    if(this.id === 'btn2') {
+        iframe.src = 'https://www.myvi.top/embed/3w7u6te7meyrxx937gshx5z74h'
+        episodeName.textContent = 'Название серии: Vanishing Children'
+        episodeOutDate.textContent = 'Дата выхода: 06.10.2012'
+    }
+    if(this.id === 'btn3') {
+        iframe.src = 'https://www.myvi.top/embed/4pzypozdmfpwifmktae3cfzyoo'
+        episodeName.textContent = 'Название серии: False Minoshiro'
+        episodeOutDate.textContent = 'Дата выхода: 13.10.2012'
+    }
+    if(this.id === 'btn4') {
+        iframe.src = 'https://www.myvi.top/embed/wg8gmgbu7bowpqychtrqfqo9ke'
+        episodeName.textContent = 'Название серии: Bloodstained History'
+        episodeOutDate.textContent = 'Дата выхода: 20.10.2012'
+    }
+    if(this.id === 'btn5') {
+        iframe.src = 'https://www.myvi.top/embed/61cze1mgzdtrjmbpzi4wm161ow'
+        episodeName.textContent = 'Название серии: A Sweltering Night on the Run'
+        episodeOutDate.textContent = 'Дата выхода: 27.10.2012'
+    }
+    if(this.id === 'btn6') {
+        iframe.src = 'https://www.myvi.top/embed/jywquytfhe1rfrrp5txjfomg1r'
+        episodeName.textContent = 'Название серии: Getaway'
+        episodeOutDate.textContent = 'Дата выхода: 04.11.2012'
+    }
+    if(this.id === 'btn7') {
+        iframe.src = 'https://www.myvi.top/embed/3hmkzzgeyk6wpqn58nctrz8sna'
+        episodeName.textContent = 'Название серии: Summer Darkness'
+        episodeOutDate.textContent = 'Дата выхода: 10.11.2012'
+    }
+    if(this.id === 'btn8') {
+        iframe.src = 'https://www.myvi.top/embed/yi77m7b1fxkwzm34bp9sdussrr'
+        episodeName.textContent = 'Название серии: Omen'
+        episodeOutDate.textContent = 'Дата выхода: 17.11.2012'
+    }
+    if(this.id === 'btn9') {
+        iframe.src = 'https://www.myvi.top/embed/t7hkspri434wpqj1d71tb1539r'
+        episodeName.textContent = 'Название серии: The Gale Came'
+        episodeOutDate.textContent = 'Дата выхода: 24.11.2012'
+    }
+    if(this.id === 'btn10') {
+        iframe.src = 'https://www.myvi.top/embed/pss3tzftbcqrfk81idkeofoz3c'
+        episodeName.textContent = 'Название серии: More Than the Darkness'
+        episodeOutDate.textContent = 'Дата выхода: 01.12.2012'
+    }
+    if(this.id === 'btn11') {
+        iframe.src = 'https://www.myvi.top/embed/q7p4ks9xxx7w9juo78d58ogq8c'
+        episodeName.textContent = `Название серии: Winter's Distant Thunder`
+        episodeOutDate.textContent = 'Дата выхода: 08.12.2012'
+    }
+    if(this.id === 'btn12') {
+        iframe.src = 'https://www.myvi.top/embed/qifygsdnmmyw7cgikkhi1pcjwc'
+        episodeName.textContent = 'Название серии: Weak Link'
+        episodeOutDate.textContent = 'Дата выхода: 15.12.2012'
+    }
+    if(this.id === 'btn13') {
+        iframe.src = 'https://www.myvi.top/embed/bpiodgmxd88wtmpw661ikt4o8e'
+        episodeName.textContent = 'Название серии: Reunion'
+        episodeOutDate.textContent = 'Дата выхода: 22.12.2012'
+    }
+    if(this.id === 'btn14') {
+        iframe.src = 'https://www.myvi.top/embed/5td94yndiinrxqby9ik4k7o3ue'
+        episodeName.textContent = 'Название серии: Snowflakes'
+        episodeOutDate.textContent = 'Дата выхода: 04.01.2013'
+    }
+    if(this.id === 'btn15') {
+        iframe.src = 'https://www.myvi.top/embed/6duf5ghome5wibbfkejjj8pf3h'
+        episodeName.textContent = 'Название серии: Afterimage'
+        episodeOutDate.textContent = 'Дата выхода: 12.01.2013'
+    }
+    if(this.id === 'btn16') {
+        iframe.src = 'https://www.myvi.top/embed/p7toq6y7xdgw5xyczd36fbj3jc'
+        episodeName.textContent = 'Название серии: To Saki, My Love'
+        episodeOutDate.textContent = 'Дата выхода: 19.01.2013'
+    }
+    if(this.id === 'btn17') {
+        iframe.src = 'https://www.myvi.top/embed/5rksa4d171xw8pn8o1k8y3ttxe'
+        episodeName.textContent = 'Название серии: Footsteps of Ruin'
+        episodeOutDate.textContent = 'Дата выхода: 26.01.2013'
+    }
+    if(this.id === 'btn18') {
+        iframe.src = 'https://www.myvi.top/embed/abg7h6dnf6gr7yg84mmkdr5sfo'
+        episodeName.textContent = 'Название серии: A Crimson Flower'
+        episodeOutDate.textContent = 'Дата выхода: 02.02.2013'
+    }
+    if(this.id === 'btn19') {
+        iframe.src = 'https://www.myvi.top/embed/4yb9bte53wfwuxdx6tsfma8zmr'
+        episodeName.textContent = 'Название серии: Darkness'
+        episodeOutDate.textContent = 'Дата выхода: 09.02.2013'
+    }
+    if(this.id === 'btn20') {
+        iframe.src = 'https://www.myvi.top/embed/uee5dktker8w3jbi5ok4ojwpka'
+        episodeName.textContent = 'Название серии: Drenched in Cold Sunlight'
+        episodeOutDate.textContent = 'Дата выхода: 16.02.2013'
+    }
+    if(this.id === 'btn21') {
+        iframe.src = 'https://www.myvi.top/embed/j5k7zq3gw6xrm8iomxumw3k55r'
+        episodeName.textContent = 'Название серии: Fires of the Apocalypse'
+        episodeOutDate.textContent = 'Дата выхода: 23.02.2013'
+    }
+    if(this.id === 'btn22') {
+        iframe.src = 'https://www.myvi.top/embed/p3cri95xyehwxgwowcxdsu7cwe'
+        episodeName.textContent = 'Название серии: Tokyo'
+        episodeOutDate.textContent = 'Дата выхода: 02.03.2013'
+    }
+    if(this.id === 'btn23') {
+        iframe.src = 'https://www.myvi.top/embed/64kkyo1u5q7rtfek5s7hnynjhh'
+        episodeName.textContent = 'Название серии: The Face of the Boy'
+        episodeOutDate.textContent = 'Дата выхода: 09.03.2013'
+    }
+    if(this.id === 'btn24') {
+        iframe.src = 'https://www.myvi.top/embed/43bqngjfqjjrmkpdi89wnfj1tc'
+        episodeName.textContent = 'Название серии: The Bonfires in the Darkness'
+        episodeOutDate.textContent = 'Дата выхода: 16.03.2013'
+    }
+    if(this.id === 'btn25') {
+        iframe.src = 'https://www.myvi.top/embed/xs8ybw8fjiewdejrftp9qp5rpo'
+        episodeName.textContent = 'Название серии: From the New World'
+        episodeOutDate.textContent = 'Дата выхода: 23.03.2013'
+    }
+}
+
+    //Сброс цвета на #2C2F33
+    document.querySelectorAll('.btn').forEach((button) => {
+        if(button.style.background === '#2C2F33') return
+        button.style.background = '#2C2F33'
+    })
+    document.querySelectorAll('.btn1').forEach((button) => {
+        if(button.style.background === '#2C2F33') return
+        button.style.background = '#2C2F33'
+    })
+    this.style.background = 'fuchsia'
+}
+let btn = document.querySelectorAll('button')
+btn.forEach(function(button) {
+    if(button.id === 'soc-button') return false
+    button.onclick = changingColorOfButtons
+})
+
+//Показ выполнения загрузки скрипта
+setTimeout(() => {
+    console.log(`Скрипт сайта загружен!`)
+}, 1000)
