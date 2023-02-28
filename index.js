358c22897e81d04b0710218e7117ceba6e5d2db8
QSSharky <sharky.2019@mail.ru>
2023-03-01 00:58:17 +0700

    Добавил .env

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const token = 'NzkxMTA1NTQwMzUxOTgzNjI2.X-KUcg.8EGLw_SiGqNOJZlcd8niE_xE2K4'
+const token = process.env.TOKEN
 const url = `https://discord.com/api/v9/channels/channel-id/messages`
 fetch(url, {
     method: 'POST',
