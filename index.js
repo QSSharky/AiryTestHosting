d480affe5972c300f2a06c1d08e292982328ce7a
QSSharky <sharky.2019@mail.ru>
2023-03-01 02:08:21 +0700

    Финальный фикс.

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import dotenv from 'dotenv'
+const token = process.env.DISCORD_TOKEN
+const url = 'https://discord.com/api/v10/channels/758471672478302211/messages'
 
-dotenv.config()
-
-const token = process.env.TOKEN
-const url = `https://discord.com/api/v10/channels/758471672478302211/messages`
-
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Bot' + token,
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-    content: 'Тест Хостинга «GitHub Pages»!'
-    })
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error))
+const request = new XMLHttpRequest()
+request.open('POST', url)
+request.setRequestHeader('Authorization', 'Bot ' + token)
+request.setRequestHeader('Content-Type', 'application/json')
+request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            console.log(request.responseText)
+        } else {
+            console.error('Error:', request.status, request.statusText)
+        }
+    }
+}
+const data = { content: 'Тест Хостинга «GitHub Pages»!' }
+request.send(JSON.stringify(data))
